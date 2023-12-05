e380aebccbaf3aa91dd56f4817377231bd5109c2
novast <93687496+northernvast@users.noreply.github.com>
2023-12-05 18:45:57 +0900

    Create relic.js

---
+++ relic.js
@@ -0,0 +1,150 @@
+const Stat {
+    // Base Stats
+    FLAT_HP: { name: "HP", scale: 0 },
+    FLAT_ATK: { name: "ATK", scale: 0 },
+    FLAT_DEF: { name: "DEF", scale: 0 },
+    HP: { name: "HP%", scale: 1 },
+    ATK: { name: "ATK%", scale: 1 },
+    DEF: { name: "DEF%", scale: 1 },
+    SPD: { name: "SPD", scale: 0 },
+    // Advanced Stats
+    CRIT_RATE: { name: "CRIT Rate%", scale: 1 },
+    CRIT_DMG: { name: "CRIT DMG%", scale: 1 },
+    BREAK_EFFECT: { name: "Break Effect%", scale: 1 },
+    OUTGOING_HEALING_BOOST: { name: "Outgoing Healing Boost", scale: 1 },
+    ENERGY_REGENERATION_RATE: { name: "Energy Regeneration Rate", scale: 1 },
+    EFFECT_HIT_RATE: { name: "Effect Hit Rate%", scale: 1 },
+    EFFECT_RES: { name: "Effect RES%", scale: 1 },
+    ELEMENTAL_DMG_BOOST: { name: "Elemental DMG Boost", scale: 1},
+}
+
+class MainStat {
+    constructor(stat, value) {
+        this.stat = stat;
+        this.value = value;
+    }
+    get formattedValue() { format(value, stat.scale); }
+}
+
+const mainStats = [
+    new MainStat(Stat.SPD, 25.032),
+    new MainStat(Stat.FLAT_HP, 705.6),
+    new MainStat(Stat.FLAT_ATK, 352.8),
+    new MainStat(Stat.HP, 43.2),
+    new MainStat(Stat.ATK, 43.2),
+    new MainStat(Stat.DEF, 54),
+    new MainStat(Stat.BREAK_EFFECT, 64.8),
+    new MainStat(Stat.EFFECT_HIT_RATE, 43.2),
+    new MainStat(Stat.ENERGY_REGENERATION_RATE, 19.4394),
+    new MainStat(Stat.OUTGOING_HEALING_BOOST, 34.5606),
+    new MainStat(Stat.ELEMENTAL_DMG_BOOST, 38.8803),
+    new MainStat(Stat.CRIT_RATE, 32.4),
+    new MainStat(Stat.CRIT_DMG, 64.8)
+];
+
+class SubStat {
+    constructor(stat, log) {
+        this.stat = stat;
+        this.log = log;
+    }
+
+    get value() {
+        let v = 0;
+        let power = 1000;
+        SubStat.getInitialValues(this.stat).forEach((val, idx) => {
+            v += power * val * this.log[idx];
+        });
+        return Math.floor(v) / power;
+    }
+
+    // return value in game
+    get formattedValue() { return format(this.value, this.stat.scale); }
+
+    equals(o) { return this.stat == o.stat && sum(this.log) == sum(o.log) && this.value == o.value; }
+    
+    // get values for initial or increase
+    static getInitialValues(stat) {
+        switch (stat) {
+            case Stat.FLAT_HP: return [33.87, 38.103755, 42.33751];
+            case Stat.FLAT_ATK: return [16.935, 19.051877, 21.168754];
+            case Stat.FLAT_DEF: return [16.935, 19.051877, 21.168754];
+            case Stat.HP: return [3.456, 3.888, 4.32];
+            case Stat.ATK: return [3.456, 3.888, 4.32];
+            case Stat.DEF: return [4.32, 4.86, 5.4];
+            case Stat.SPD: return [2.0, 2.3, 2.6];
+            case Stat.CRIT_RATE: return [2.592, 2.916, 3.24];
+            case Stat.CRIT_DMG: return [5.184, 5.832, 6.48];
+            case Stat.BREAK_EFFECT: return [5.184, 5.832, 6.48];
+            case Stat.EFFECT_HIT_RATE: return [3.456, 3.888, 4.32];
+            case Stat.EFFECT_RES: return [3.456, 3.888, 4.32];
+            default: return undefined;
+        }
+    }
+}
+
+class PossibleSubStat {
+    constructor(stat) {
+        this.all = PossibleRelicSubStat.#COMBINATIONS
+                .map(it => new RelicSubStat(stat, it))
+                .sort((a, b) => a.value - b.value);
+    }
+
+    get min() { return first(this.all); }
+    get max() { return last(this.all); }
+
+    static #COMBINATIONS = [ // 83 combinations
+        [1,0,0],[0,1,0],[0,0,1],[2,0,0],[1,1,0],[1,0,1],[0,2,0],[0,1,1],[0,0,2],[3,0,0],
+        [2,1,0],[2,0,1],[1,2,0],[1,1,1],[1,0,2],[0,3,0],[0,2,1],[0,1,2],[0,0,3],[4,0,0],
+        [3,1,0],[3,0,1],[2,2,0],[2,1,1],[2,0,2],[1,3,0],[1,2,1],[1,1,2],[1,0,3],[0,4,0],
+        [0,3,1],[0,2,2],[0,1,3],[0,0,4],[5,0,0],[4,1,0],[4,0,1],[3,2,0],[3,1,1],[3,0,2],
+        [2,3,0],[2,2,1],[2,1,2],[2,0,3],[1,4,0],[1,3,1],[1,2,2],[1,1,3],[1,0,4],[0,5,0],
+        [0,4,1],[0,3,2],[0,2,3],[0,1,4],[0,0,5],[6,0,0],[5,1,0],[5,0,1],[4,2,0],[4,1,1],
+        [4,0,2],[3,3,0],[3,2,1],[3,1,2],[3,0,3],[2,4,0],[2,3,1],[2,2,2],[2,1,3],[2,0,4],
+        [1,5,0],[1,4,1],[1,3,2],[1,2,3],[1,1,4],[1,0,5],[0,6,0],[0,5,1],[0,4,2],[0,3,3],
+        [0,2,4],[0,1,5],[0,0,6]
+    ];
+}
+
+// data
+
+const data = [
+    Stat.FLAT_HP,
+    Stat.FLAT_ATK,
+    Stat.FLAT_DEF,
+    Stat.HP,
+    Stat.ATK,
+    Stat.DEF,
+    Stat.SPD,
+    Stat.CRIT_RATE,
+    Stat.CRIT_DMG,
+    Stat.BREAK_EFFECT,
+    Stat.EFFECT_HIT_RATE,
+    Stat.EFFECT_RES
+].forEach(it => new PossibleSubStat(it));
+
+// utility functions
+
+function format(number, scale) {
+    if (scale == 0) {
+        return Math.floor(number).toString();
+    }
+    let s = Math.floor(number * Math.pow(10, scale)).toString();
+    let p = s.length - scale;
+    return s.slice(0, p) + "." + s.slice(p);
+}
+
+function sum(numbers) {
+    return numbers.reduce((a,b) => a + b, 0);
+}
+
+function first(array) {
+    return array[0];
+}
+
+function last(array) {
+    return array[array.length - 1];
+}
+
+function deduplicate(array) {
+    return [...new Set(array)];
+}

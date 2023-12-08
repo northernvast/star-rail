5c49db9ce92f0b6bc74a2b60b498caa35904293c
novast <93687496+northernvast@users.noreply.github.com>
2023-12-08 18:30:38 +0900

    Update relic.js

--- relic.js
+++ relic.js
@@ -1,22 +1,22 @@
-// instead of enum class
+// Relic Stat Type
 const Stat = {
     // Base Stats
     FLAT_HP: { name: "HP", scale: 0 },
-    FLAT_ATK: { name: "ATK", scale: 0 },
-    FLAT_DEF: { name: "DEF", scale: 0 },
-    HP: { name: "HP%", scale: 1 },
-    ATK: { name: "ATK%", scale: 1 },
-    DEF: { name: "DEF%", scale: 1 },
-    SPD: { name: "SPD", scale: 0 },
+    FLAT_ATK: { name: "攻撃力", scale: 0 },
+    FLAT_DEF: { name: "防御力", scale: 0 },
+    HP: { name: "HP(%)", scale: 1 },
+    ATK: { name: "攻撃力(%)", scale: 1 },
+    DEF: { name: "防御力(%)", scale: 1 },
+    SPD: { name: "速度", scale: 0 },
     // Advanced Stats
-    CRIT_RATE: { name: "CRIT Rate%", scale: 1 },
-    CRIT_DMG: { name: "CRIT DMG%", scale: 1 },
-    BREAK_EFFECT: { name: "Break Effect%", scale: 1 },
-//    OUTGOING_HEALING_BOOST: { name: "Outgoing Healing Boost", scale: 1 },
-//    ENERGY_REGENERATION_RATE: { name: "Energy Regeneration Rate", scale: 1 },
-    EFFECT_HIT_RATE: { name: "Effect Hit Rate%", scale: 1 },
-    EFFECT_RES: { name: "Effect RES%", scale: 1 },
-//    ELEMENTAL_DMG_BOOST: { name: "Elemental DMG Boost", scale: 1},
+    CRIT_RATE: { name: "会心率(%)", scale: 1 },
+    CRIT_DMG: { name: "会心ダメージ(%)", scale: 1 },
+    BREAK_EFFECT: { name: "撃破特攻(%)", scale: 1 },
+    OUTGOING_HEALING_BOOST: { name: "Outgoing Healing Boost", scale: 1 },
+    ENERGY_REGENERATION_RATE: { name: "Energy Regeneration Rate", scale: 1 },
+    EFFECT_HIT_RATE: { name: "効果命中(%)", scale: 1 },
+    EFFECT_RES: { name: "効果抵抗(%)", scale: 1 },
+    ELEMENTAL_DMG_BOOST: { name: "Elemental DMG Boost", scale: 1},
 }
 
 class SubStat {
@@ -59,46 +59,6 @@ class SubStat {
     }
 }
 
-class PossibleSubStat {
-    constructor(stat) {
-        this.all = PossibleSubStat.#COMBINATIONS
-                .map(it => new SubStat(stat, it))
-                .sort((a, b) => a.value - b.value);
-    }
-
-    get min() { return first(this.all); }
-    get max() { return last(this.all); }
-
-    static #COMBINATIONS = [ // 83 combinations
-        [1,0,0],[0,1,0],[0,0,1],[2,0,0],[1,1,0],[1,0,1],[0,2,0],[0,1,1],[0,0,2],[3,0,0],
-        [2,1,0],[2,0,1],[1,2,0],[1,1,1],[1,0,2],[0,3,0],[0,2,1],[0,1,2],[0,0,3],[4,0,0],
-        [3,1,0],[3,0,1],[2,2,0],[2,1,1],[2,0,2],[1,3,0],[1,2,1],[1,1,2],[1,0,3],[0,4,0],
-        [0,3,1],[0,2,2],[0,1,3],[0,0,4],[5,0,0],[4,1,0],[4,0,1],[3,2,0],[3,1,1],[3,0,2],
-        [2,3,0],[2,2,1],[2,1,2],[2,0,3],[1,4,0],[1,3,1],[1,2,2],[1,1,3],[1,0,4],[0,5,0],
-        [0,4,1],[0,3,2],[0,2,3],[0,1,4],[0,0,5],[6,0,0],[5,1,0],[5,0,1],[4,2,0],[4,1,1],
-        [4,0,2],[3,3,0],[3,2,1],[3,1,2],[3,0,3],[2,4,0],[2,3,1],[2,2,2],[2,1,3],[2,0,4],
-        [1,5,0],[1,4,1],[1,3,2],[1,2,3],[1,1,4],[1,0,5],[0,6,0],[0,5,1],[0,4,2],[0,3,3],
-        [0,2,4],[0,1,5],[0,0,6]
-    ];
-}
-
-// data
-
-const possibleSubStats = [
-    Stat.FLAT_HP,
-    Stat.FLAT_ATK,
-    Stat.FLAT_DEF,
-    Stat.HP,
-    Stat.ATK,
-    Stat.DEF,
-    Stat.SPD,
-    Stat.CRIT_RATE,
-    Stat.CRIT_DMG,
-    Stat.BREAK_EFFECT,
-    Stat.EFFECT_HIT_RATE,
-    Stat.EFFECT_RES
-].map(it => new PossibleSubStat(it));
-
 // utility functions
 
 function format(number, scale) {
@@ -110,18 +70,10 @@ function format(number, scale) {
     return s.slice(0, p) + "." + s.slice(p);
 }
 
-function sum(numbers) {
-    return numbers.reduce((a,b) => a + b, 0);
-}
+function sum(array) { return array.reduce((a,b) => a + b, 0); }
 
-function first(array) {
-    return array[0];
-}
+function first(array) { return array[0]; }
 
-function last(array) {
-    return array[array.length - 1];
-}
+function last(array) { return array[array.length - 1]; }
 
-function deduplicate(array) {
-    return [...new Set(array)];
-}
+function deduplicate(array) { return [...new Set(array)]; }
